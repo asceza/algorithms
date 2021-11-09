6b620d4b25205d83b72f2ea2caf8591c515a674c
wezochy <constanta.john@gmail.com>
2021-11-09 20:40:44 +0300

    Add comment to 1_bubble_sort.py

--- 1_bubble_sort.py
+++ 1_bubble_sort.py
@@ -1,6 +1,7 @@
 # сортировка пузырьком
 # thanks to -> https://www.youtube.com/watch?v=5BuCMzKYagg
 # https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%BF%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC
+# https://airtucha.github.io/SortVis/
 # вычислительная сложность O(N^2)
 
 lst = [-14, 2, 0, 12, 9, -2]
